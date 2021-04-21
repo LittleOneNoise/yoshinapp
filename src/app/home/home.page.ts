bc2846b4e46869ab5e5b5f90ed7725e95906b24d
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2021-04-21 13:17:44 +0200

    update bug tabs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,12 @@ export class HomePage {
 
   constructor() {}
 
+  goToLearningHiragana(){
+    console.log("Clicked 1rst menu button");
+  }
+
+  goToLearning(){
+    console.log("Clicked first tab bottom nav");
+  }
+
 }
