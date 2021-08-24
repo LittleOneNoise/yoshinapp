f84cd130c2822766922b42574bfb064eae2aa085
Little One <71726170+LittleOneNoise@users.noreply.github.com>
2021-08-25 01:49:13 +0200

    Mnemonics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+       console.log("app initialized !!")
+        SplashScreen.hide({
+          fadeOutDuration: 1000
+        });
+    });
+}
+
 }
